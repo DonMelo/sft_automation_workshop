500d320481abe2039328b98c9cbb4ab5bdfbbfb7
Erika <kamicaityt@gmail.com>
2025-07-24 17:08:40 +0300

    first task with automation and unit tests

---
+++ src/example/example.js
@@ -0,0 +1,7 @@
+let addNumbers = (number, secondNumber) => {
+    if((typeof number == 'number') && (typeof secondNumber == 'number')) {
+        return number + secondNumber
+    }
+    return undefined;
+}
+module.exports = addNumbers;

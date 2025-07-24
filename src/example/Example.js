191408d7cf573f5062f38e045f2b6694c2b11782
Donatas Misevicius <Donatas.misevicius@cognizant.com>
2025-07-24 14:01:08 +0300

    adding leftover code

---
+++ src/example/Example.js
@@ -0,0 +1,8 @@
+let addition = (number, secondNumber) => {
+    if((typeof number == 'number') && (typeof secondNumber == 'number')) {
+        return number + secondNumber;    
+    }
+    return undefined;
+}
+
+module.exports = addition;
---
+++ src/unit-tests/example.test.js
@@ -0,0 +1,6 @@
+const {it, expect} = require('@jest/globals');
+const addition = require('../example/Example');
+
+it.each([[1,2,3], ['a','b', undefined ]])('should add two numbers and not able to add letters', async (firstNumber, secondNumber, answer) => {
+    expect(addition(firstNumber,secondNumber)).toBe(answer);
+})

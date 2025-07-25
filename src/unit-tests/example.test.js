b038527d72e9686540c2f0056d427d521ce946dd
Sarune <sarune.van@gmail.com>
2025-07-25 12:17:27 +0300

    Workshop Files

---
+++ src/unit-tests/example.test.js
@@ -0,0 +1,6 @@
+const {it, expect} = require('@jest/globals');
+const addition = require('../example/Example');
+
+    it.each([[1,2,3], ['a', 'b', undefined]]) ('should add two numbers and not able to add letters', async(firstnumber, secondnumber, answer) => {
+    expect(addition(firstnumber, secondnumber)).toBe(answer);
+    })

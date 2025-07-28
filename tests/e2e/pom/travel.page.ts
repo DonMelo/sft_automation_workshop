51efdbad8c0b1d10644fb4398dbedac6f50705b0
jurgitajaz@gmail.com <jurgitajaz@gmail.com>
2025-07-28 14:51:57 +0300

    Added E2E tests for travel page

--- tests/e2e/pom/travel.page.ts
+++ tests/e2e/pom/travel.page.ts
@@ -1,40 +1,109 @@
-import {expect, Page} from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 export class TravelPage {
-    readonly page: Page;
+  readonly page: Page;
 
-    constructor(page: Page){
-        this.page = page;
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    async navigateToPage() {
-        await this.page.goto('https://travel.agileway.net');   
-    }
+  async navigateToPage() {
+    await this.page.goto('https://travel.agileway.net');
+  }
 
-    // get input by ID and focus on it
-    async getInputById(id: string) {
-        return this.page.locator(`#${id}`);
-    }
+  async login(username: string, password: string) {
+    await this.page.locator('#username').fill(username);
+    await this.page.locator('#password').fill(password);
+    await this.pressSighinButton();
+  }
 
-    // type into the input value
-    async typeIntoInput(id: string, value: string) {
-        const input = await this.getInputById(id);
-        await input.fill(value);
-    }
+  async pressSighinButton() {
+    await this.page.locator('input[type=submit]', { hasText: 'Sign in' }).click();
+  }
 
-    // check if input value contains the expected value
-    async validateInput(id: string, expectedValue: string) {
-        const input = await this.getInputById(id);
-        await expect(input).toHaveValue(expectedValue);
-    }
+  async verifyThatUserLoggedIn() {
+    await expect(this.page.locator('#flash_notice')).toHaveText('Signed in!');
+  }
 
-    // validate text in the block
-    async validateTextInInput(id: string, expectedText: string) {
-        const input = await this.getInputById(id);
-        await expect(input).toHaveText(expectedText);
-    }
+  async selectOneWayFlight(from: string, to: string, departingDay: string, departingMonth: string) {
+    await this.page.locator('[name="fromPort"]').selectOption(from);
+    await this.page.locator('[name="toPort"]').selectOption(to);
+    await this.page.locator('[name="departDay"]').selectOption(departingDay);
+    await this.page.locator('[name="departMonth"]').selectOption(departingMonth);
+    await this.pressContinueButton();
+  }
 
-    async clickEnterButton() {
-        await this.page.keyboard.press('Enter');
-    }
+  async selectreturnFlight(from: string, to: string, departingDay: string, departingMonth: string, returnDay: string, returnMonth: string) {
+    await this.page.locator('[name="fromPort"]').selectOption(from);
+    await this.page.locator('[name="toPort"]').selectOption(to);
+    await this.page.locator('[name="departDay"]').selectOption(departingDay);
+    await this.page.locator('[name="departMonth"]').selectOption(departingMonth);
+    await this.page.locator('[name="returnDay"]').selectOption(returnDay);
+    await this.page.locator('[name="returnMonth"]').selectOption(returnMonth);
+    await this.pressContinueButton();
+  }
+
+  async pressContinueButton() {
+    await this.page.locator('input[type=submit]', { hasText: 'Continue' }).click();
+  }
+
+  async fillInPassangerDetails(firstName: string, lastName: string) {
+    await this.page.locator('[name="passengerFirstName"]').fill(firstName);
+    await this.page.locator('[name="passengerLastName"]').fill(lastName);
+    await this.pressnextButton();
+  }
+
+  async pressnextButton() {
+    await this.page.locator('input[type=submit]', { hasText: 'Next' }).click();
+  }
+
+  async fillInCreditCardForm(cardType: string, cardNumber: string, expireDay: string, expireYear: string) {
+    await this.page.locator(`[name="card_type"][value="${cardType}"]`).check();
+    await this.page.locator('[name="card_number"]').fill(cardNumber);
+    await this.page.locator('[name="expiry_month"]').selectOption(expireDay);
+    await this.page.locator('[name="expiry_year"]').selectOption(expireYear);
+    await this.presspaynow();
+  }
+
+  async presspaynow() {
+    await this.page.locator('input[type=submit]', { hasText: 'Pay now' }).click();
+  }
+
+  async verifyBookingInformationOneWay(from: string, to: string, departingDay: string, departingMonth: string, firstName: string, lastName: string) {
+    await expect(this.page.locator('#booking_number')).toBeVisible();
+    const confirmationText = await this.page.locator('#confirmation').innerText();
+
+    const departureYear = departingMonth.slice(2, 6);
+    const departureMonth = departingMonth.slice(0, 2);
+    const departDate = `${departureYear}-${departureMonth}-${departingDay}`;
+
+    await expect(confirmationText).toContain('(oneway Trip)');
+    await expect(confirmationText).toContain(`${departDate} ${from} to ${to}`);
+    await expect(confirmationText).toContain(`Passenger Details: ${firstName} ${lastName}`);
+  }
+
+  async verifyBookingInformationReturn(from: string, to: string, departingDay: string, departingMonth: string, returnDay: string, returnMonth: string, firstName: string, lastName: string) {
+    await expect(this.page.locator('#booking_number')).toBeVisible();
+    const confirmationText = await this.page.locator('#confirmation').innerText();
+
+    const departDate = `${departingMonth.slice(2, 6)}-${departingMonth.slice(0, 2)}-${departingDay}`;
+    const returnDate = `${returnMonth.slice(2, 6)}-${returnMonth.slice(0, 2)}-${returnDay}`;
+
+    await expect(confirmationText).toContain('(return Trip)');
+    await expect(confirmationText).toContain(`${departDate} ${from} to ${to}`);
+    await expect(confirmationText).toContain(`${returnDate} ${to} to ${from}`);
+    await expect(confirmationText).toContain(`Passenger Details: ${firstName} ${lastName}`);
+  }
+
+  async signoff() {
+    await this.page.locator('#user_nav > a', { hasText: 'Sign off' }).click();
+  }
+
+  async verifyThatUsersignedoff() {
+    await expect(this.page.locator('#flash_notice')).toHaveText('Signed out!');
+  }
+
+  async verifyThatLastNameRequired() {
+    await expect(this.page.locator('#flash_alert')).toHaveText('Must provide last name');
+  }
 }
--- tests/e2e/travel-page.spec.ts
+++ tests/e2e/travel-page.spec.ts
@@ -1,24 +1,98 @@
 import { test, expect } from '@playwright/test';
 import { TravelPage } from './pom/travel.page';
 
+// I chose to automate the main user actions: logging in, booking one-way and return flights, and logging out. 
+// These are important features that can easily break if the system changes. I also added two negative 
+// tests: one for wrong login details and one for a missing last name. 
+// These tests help check that the system properly handles user input. 
+// Overall, the tests make sure that the most important parts of the app keep working after updates.
+
 test.describe('Travel Page', () => {
     test.beforeEach(async ({ page }) => {
-        // Setup before each test: navigate to the ToDo page
+        // Setup before each test: navigate to the Travel page
         const travelPage = new TravelPage(page);
         await travelPage.navigateToPage();
     });
 
-    test('should login', async ({ page }) => {
+    test('should show error when credentials are wrong', async ({ page }) => { //Login (invalid credentials)
         const travelPage = new TravelPage(page);
 
-        await travelPage.typeIntoInput('username', 'agileway');
-        await travelPage.validateInput('username', 'agileway');
-
-        await travelPage.typeIntoInput('password', 'testW1se');
-        await travelPage.validateInput('password', 'testW1se');
-   
-        await travelPage.clickEnterButton();
-
-        await travelPage.validateTextInInput('flash_notice', 'Signed in!x');
+        await travelPage.login('agileway','neteisingasslaptazodis');
+        
+        await expect(travelPage.page.locator('#flash_alert')).toHaveText('Invalid email or password');
     });
-});
+
+    test('should login', async ({ page }) => { //Login (valid credentials)
+        const travelPage = new TravelPage(page);
+        
+        await travelPage.login('agileway','testW1se');
+
+        await travelPage.verifyThatUserLoggedIn();
+    });
+
+
+    test('should login and select oneway flight', async ({ page }) => { //One-Way flight booking 
+        const travelPage = new TravelPage(page);
+
+        await travelPage.login('agileway','testW1se');
+        await travelPage.verifyThatUserLoggedIn();
+
+        await travelPage.page.locator('input[type="radio"][name="tripType"][value="oneway"]').check();
+        await travelPage.selectOneWayFlight('New York', 'Sydney', '02', '072025');
+        
+        await travelPage.fillInPassangerDetails('Jurgita', 'Lizdene');
+  
+        await travelPage.fillInCreditCardForm('visa', '1234 1234', '02', '2028');
+        
+        await travelPage.verifyBookingInformationOneWay('New York', 'Sydney', '02', '072025', 'Jurgita', 'Lizdene');
+
+
+    });
+
+     test('should login and select return flight', async ({ page }) => { //Return flight booking 
+        const travelPage = new TravelPage(page);
+
+        await travelPage.login('agileway','testW1se');
+        await travelPage.verifyThatUserLoggedIn();
+
+        await travelPage.page.locator('input[type="radio"][name="tripType"][value="return"]').check();
+        await travelPage.selectreturnFlight('New York', 'Sydney', '02', '072025', '03', '082025');
+        
+        await travelPage.fillInPassangerDetails('Jurgita', 'Lizdene');
+  
+        await travelPage.fillInCreditCardForm('visa', '1234 1234', '02', '2028');
+        
+        await travelPage.verifyBookingInformationReturn('New York', 'Sydney', '02', '072025', '03', '082025', 'Jurgita', 'Lizdene');
+
+
+    });
+
+     test('should sign off', async ({ page }) => { //Sign-off (Logout)
+        const travelPage = new TravelPage(page);
+
+        await travelPage.login('agileway','testW1se');
+        await travelPage.verifyThatUserLoggedIn();
+
+        await travelPage.signoff();
+        await travelPage.verifyThatUsersignedoff();
+    });
+
+    test('Last name required', async ({ page }) => { //Required field validation (Last name)
+        const travelPage = new TravelPage(page);
+
+        await travelPage.login('agileway','testW1se');
+        await travelPage.verifyThatUserLoggedIn();
+        
+        await travelPage.page.locator('input[type="radio"][name="tripType"][value="oneway"]').check();
+        await travelPage.selectOneWayFlight('New York', 'Sydney', '02', '072025');
+        
+        await travelPage.fillInPassangerDetails('Jurgita','');
+        await travelPage.verifyThatLastNameRequired();
+     });
+}); 
+
+
+ 
+
+
+

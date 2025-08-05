ae038a5b116b94785852b1bcdb36696d38c677a2
Mariu3e <marius.kursai@gmail.com>
2025-08-05 12:09:38 +0300

    Updated homework

--- tests/e2e/travel.spec.ts
+++ tests/e2e/travel.spec.ts
@@ -1,53 +1,49 @@
-import { test, expect } from '@playwright/test';
-import { TravelPage } from 'e2e-tests/pom/travel.page';
+import { test, expect } from "@playwright/test";
+import { TravelPage } from "e2e-tests/pom/travel.page";
 
-let travelPage : TravelPage;
+let travelPage: TravelPage;
 
-test.beforeEach('setup', async ({page}) => {
-    travelPage = new TravelPage(page);
+test.beforeEach("setup", async ({ page }) => {
+  travelPage = new TravelPage(page);
 
   await travelPage.goTo();
 });
 
 const validCredentials = [
   {
-   validUsername: 'agileway', 
-   validPassword: 'testW1se'
-  }
+    validUsername: "agileway",
+    validPassword: "testW1se",
+  },
 ];
 const invalidCredentials = [
   {
-   invalidUsername: 'agilebay', 
-   invalidPassword: 'testW1se'
-  }
- ];
- const emptyCredentials = [
+    invalidUsername: "agilebay",
+    invalidPassword: "testW1se",
+  },
+];
+const emptyCredentials = [
   {
-   emptyUsername: '', 
-   emptyPassword: ''
-  }
+    emptyUsername: "",
+    emptyPassword: "",
+  },
 ];
 
-for (const{validUsername, validPassword} of validCredentials) {
+for (const { validUsername, validPassword } of validCredentials) {
   test("Should login with valid credentials", async ({ page }) => {
     await travelPage.validLogin(validUsername, validPassword);
-    await expect(page.locator('#flash_notice')).toHaveText('Signed in!');
+    await expect(page.locator("#flash_notice")).toHaveText("Signed in!");
   });
 }
 
-for (const{invalidUsername, invalidPassword} of invalidCredentials) {
-  test("Shouldn't login with invalid credentials", async ({ page }) => {  
+for (const { invalidUsername, invalidPassword } of invalidCredentials) {
+  test("Shouldn't login with invalid credentials", async ({ page }) => {
     await travelPage.invalidLogin(invalidUsername, invalidPassword);
-    await expect(page.locator('#flash_alert')).toHaveText('Invalid email or password');
+    await expect(page.locator("#flash_alert")).toHaveText("Invalid email or password");
   });
 }
-for (const{emptyUsername, emptyPassword} of emptyCredentials) {
+for (const { emptyUsername, emptyPassword } of emptyCredentials) {
   test("Shouldn't login with empty credentials", async ({ page }) => {
     await travelPage.emptyLogin(emptyUsername, emptyPassword);
-    await expect(page.locator('#flash_alert')).toHaveText('Invalid email or password');
+    await expect(page.locator("#flash_alert")).toHaveText("Invalid email or password");
   });
 }
-
-
-
-
--- tests/pom/travel.page.ts
+++ tests/pom/travel.page.ts
@@ -1,50 +1,46 @@
-import { Locator, Page, expect} from '@playwright/test';
+import { Locator, Page, expect } from "@playwright/test";
 
 export class TravelPage {
-    readonly page: Page;
-    readonly username: Locator;
-    readonly password: Locator;
-    readonly buttonSignIn: Locator;
+  readonly page: Page;
+  readonly username: Locator;
+  readonly password: Locator;
+  readonly buttonSignIn: Locator;
 
+  constructor(page: Page) {
+    this.page = page;
+    this.username = page.locator("#username");
+    this.password = page.locator("#password");
+    this.buttonSignIn = page.getByRole("button", { name: "Sign in" });
+  }
 
-        constructor(page: Page) {
-            this.page = page;
-            this.username = page.locator('#username');
-            this.password = page.locator('#password');
-            this.buttonSignIn = page.getByRole('button', { name: 'Sign in' });
-            
-        }
+  async goTo() {
+    await this.page.goto("https://travel.agileway.net/login");
+  }
 
-        async goTo(){
-        await this.page.goto('https://travel.agileway.net/login');
-    }
+  async validLogin(validUsername: string, validPassword: string) {
+    await this.username.click();
+    await this.username.fill(validUsername);
+    await this.password.click();
+    await this.password.fill(validPassword);
+    await this.buttonSignIn.click();
+    await this.page.waitForTimeout(300);
+  }
 
-        async validLogin(validUsername: string, validPassword: string){
-        await this.username.click();
-        await this.username.fill(validUsername);
-        await this.password.click();
-        await this.password.fill(validPassword);
-        await this.buttonSignIn.click();
-        await this.page.waitForTimeout(300);
-        }
+  async invalidLogin(invalidUsername: string, invalidPassword: string) {
+    await this.username.click();
+    await this.username.fill(invalidUsername);
+    await this.password.click();
+    await this.password.fill(invalidPassword);
+    await this.buttonSignIn.click();
+    await this.page.waitForTimeout(300);
+  }
 
-        async invalidLogin(invalidUsername: string, invalidPassword: string){
-        await this.username.click();
-        await this.username.fill(invalidUsername);
-        await this.password.click();
-        await this.password.fill(invalidPassword);
-        await this.buttonSignIn.click();
-        await this.page.waitForTimeout(300);
-        }
-
-        async emptyLogin(emptyUsername: string, emptyPassword: string){
-        await this.username.click();
-        await this.username.fill(emptyUsername);
-        await this.password.click();
-        await this.password.fill(emptyPassword);
-        await this.buttonSignIn.click();
-        await this.page.waitForTimeout(300);
-        }
-
-
+  async emptyLogin(emptyUsername: string, emptyPassword: string) {
+    await this.username.click();
+    await this.username.fill(emptyUsername);
+    await this.password.click();
+    await this.password.fill(emptyPassword);
+    await this.buttonSignIn.click();
+    await this.page.waitForTimeout(300);
+  }
 }
